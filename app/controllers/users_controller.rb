7fcd1529162b9b7ceb0fde95e747419601b535aa
Bury <mati0090@gmail.com>
2013-09-12 17:38:16 +0200

    JSON User Show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,20 @@ class UsersController < ApplicationController
   respond_to :json
 
   def index
-    respond_with(User.all)
+    respond_with users
   end
+
+  def show
+    respond_with user
+  end
+
+  private
+    def user
+      User.find(params[:id])
+    end
+
+    def users
+      User.all
+    end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,14 +2,23 @@ require 'spec_helper'
 
 describe UsersController do
 
-  it "should response with all users" do
-    bill = User.create!(:first_name => "Bill", :last_name => "Gates", :hobby => "Philanthropy")
-    bob  =User.create!(:first_name => "Bob", :last_name => "Dylan", :hobby => "Signing")
+  describe "GET requests" do
+    let!(:bill) {User.create!(:first_name => "Bill", :last_name => "Gates", :hobby => "Philanthropy")}
+    let!(:bob)  {User.create!(:first_name => "Bob", :last_name => "Dylan", :hobby => "Signing")}
 
-    get :index, format: :json
-    body = JSON.parse(response.body)
+    it "should response with all users" do
+      get :index, format: :json
+      body = JSON.parse(response.body)
 
-    body.should == [JSON.parse(bill.to_json), JSON.parse(bob.to_json)]
+      body.should == [JSON.parse(bill.to_json), JSON.parse(bob.to_json)]
+    end
+
+    it "should response for show action" do
+      get :show, :id => bill.id, :format => :json
+
+      response.body.should == bill.to_json
+    end
+
   end
 
 end
