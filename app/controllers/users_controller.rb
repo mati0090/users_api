b4bd1c4f7a91ca3f52f751cff4b1efa57ec849d4
Bury <mati0090@gmail.com>
2013-09-14 16:38:56 +0200

    Updating Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
   respond_to :json
+  before_filter :authenticate, :except => [:index, :show]
 
   def index
     respond_with users, :except => User::RESTRICTED_FIELDS
   end
 
   def show
-    respond_with user, :only => showable_fields
+    respond_with user, :only => available_fields
+  end
+
+  def update
+    user.update_attributes(params[:user])
+
+    respond_with user
   end
 
   private
@@ -18,12 +25,16 @@ class UsersController < ApplicationController
       User.all
     end
 
-    def showable_fields
+    def available_fields
       authenticated? ? User.column_names : User.column_names - User::RESTRICTED_FIELDS
     end
 
     def authenticated?
       user and user.uuid == params[:uuid] and user.secret_token == params[:secret_token]
+    end
+
+    def authenticate
+      render :status => :forbidden, :text => "You must be authenticated", :template => false if !authenticated?
     end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,6 +9,7 @@ end
 describe UsersController do
   let!(:bill) {User.create!(:first_name => "Bill", :last_name => "Gates", :hobby => "Philanthropy")}
   let!(:bob)  {User.create!(:first_name => "Bob", :last_name => "Dylan", :hobby => "Signing")}
+  let(:new_bill_attributes) { {:hobby => "Programming"} }
 
   describe "GET requests without authentication" do
 
@@ -25,6 +26,12 @@ describe UsersController do
       response.body.should == bill.to_json_without_auth
     end
 
+    it "should send 403 on update" do
+      put :update, :id => bill.id, :format => :json, :user => new_bill_attributes
+
+      response.status.should == 403
+    end
+
   end
 
   describe "authenticated requests" do
@@ -34,6 +41,15 @@ describe UsersController do
       body = JSON.parse(response.body)
 
       body.should == JSON.parse(bill.to_json)
+    end
+
+    it "should be able to UPDATE user" do
+      put :update, :id => bill.id, :uuid => bill.uuid, :secret_token => bill.secret_token, :user => new_bill_attributes, :format => :json
+
+      bill.reload
+      bill.hobby.should == "Programming"
+
+      response.status.should == 204
     end
 
   end
